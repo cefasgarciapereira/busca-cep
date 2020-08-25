3e442863b165cbcfd148704a12199605638e4c2d
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-08-25 17:26:32 -0300

    Creates the AddressForm component
    This component is a form that handles the user's address. The user can fill the form by himself or use an autofill featur, which the components search on the web by the address corresponding the informed CEP.

---
+++ src/components/AddressForm/index.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import Input from '../Input';
+import Button from '../Button';
+import Loading from '../Loading';
+import styles from './styles';
+import {getAddressByCep} from '../../utils/address';
+
+export default function AddressForm(){
+    const [address, setAddress] = useState({
+        cep: '',
+        street: '',
+        city: '',
+        uf: ''
+    });
+    const [isLoading, setIsLoading] = useState(false)
+
+    const _handleCEP = async () =>{
+        setIsLoading(true);
+        const apiAddress = await getAddressByCep(address.cep)
+        if(apiAddress){
+            setAddress({
+                ...address,
+                street: apiAddress.logradouro,
+                city: apiAddress.localidade,
+                uf: apiAddress.uf
+            })
+        }
+        setIsLoading(false);
+    }
+
+    return(
+        <View style={styles.container}>
+            <Loading loading={isLoading}/>
+            <Input
+                placeholder="CEP"
+                name='cep'
+                keyboardType='number-pad'
+                value={address.cep}
+                onChange={(text) => setAddress({...address, cep: text})}/>
+            <Input
+                placeholder="Rua"
+                value={address.street}
+                onChange={(text) => setAddress({...address, street: text})}/>
+            <Input
+                placeholder="Cidade"
+                value={address.city}
+                onChange={(text) => setAddress({...address, city: text})}/>
+            <Input
+                placeholder="Estado"
+                value={address.uf}
+                onChange={(text) => setAddress({...address, uf: text})}/>
+
+            <Button
+            label="Limpar"
+            action={() => setAddress({})}/>
+            <Button
+                label="Confirmar"
+                action={() => _handleCEP('123')}/>
+        </View>
+    )
+}
---
+++ src/components/AddressForm/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        width: '100%'
+    },
+    loading: {
+      color: 'white'
+    }
+  });
+  
+export default styles;
